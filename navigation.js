355e0fe1f364f658c231e9f2b89087c98e0eb5d7
Neha Thakur <nehathakur@Nehas-MacBook-Pro.local>
2022-02-11 21:44:06 +0530

    initial commit

---
+++ navigation.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+
+// when someone clicks the hamburger button
+
+navToggle.addEventListener("click", () => { 
+	
+	const visibility = nav.getAttribute("data-visible");
+	if (visibility == "false") {
+		// if the nav is closed, open it
+		nav.setAttribute("data-visible", true);
+		navToggle.setAttribute("aria-expanded", true);
+	} else {
+		// if the nav is open, close it
+		nav.setAttribute("data-visible", false);
+		navToggle.setAttribute("aria-expanded", false);
+	}
+});
+
